16dfcf084755661ebdb2beeac42847113ecc5f42
Jeff Toppings <jtoppings@vendasta.com>
2021-09-08 08:42:16 -0600

    feat: add sample tests, some TODO's for users

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,21 @@
 
 
-describe('First Test', () => {
+describe('Some Tests', () => {
   it('should open application', () => {
     cy.visit('http://localhost:4200/');
-  })
+  });
+
+  it('should correctly display hero when match is found when searching, and take you to details page when clicked', () => {
+    cy.visit('http://localhost:4200/');
+    cy.get('[data-testid=hero-search-input]').type('Tornado');
+    cy.get('[data-testid=hero-item-0]').click();
+    cy.url().should('eq', 'http://localhost:4200/detail/20');
+  });
+
+  it('should clear messages when button is clicked', () => {
+    cy.visit('http://localhost:4200/');
+    cy.get('[data-testid=hero-search-input]').type('Fake Hero');
+
+    // TODO: complete test
+  });
 });
